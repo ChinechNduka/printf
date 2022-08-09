60d67eb5ccb33cfc5e3c332174c3876a720756c1
peternmacharia <peternmacharia@outlook.com>
2022-08-09 22:40:43 +0300

    Reverting Changes to _printf and main files commit #2

--- _printf.c
+++ _printf.c
@@ -1,24 +1,45 @@
 #include "main.h"
-#include <stdarg.h>
-
 /**
- * _printf - a function that prints anything
- * @format: identifier to look for
- *
- * Return: the number of characters printed
+ * _printf - is a function that selects the correct function to print.
+ * @format: identifier to look for.
+ * Return: the length of the string.
  */
-
-int _printf(const char *format, ...)
+int _printf(const char * const format, ...)
 {
-    int i = 0;
-    int printed = 0;
+	convert_match m[] = {
+		{"%s", printf_string}, {"%c", printf_char},
+		{"%%", printf_37},
+		{"%i", printf_int}, {"%d", printf_dec}, {"%%r", printf_srev},
+		{"%%R", printf_rot13}, {"%%b", printf_bin},
+		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_hex},
+		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
+	};
 
-    while (format[i])
-    {
-        _putchar(format[i]);
-        printed++;
-        i++;
-    }
+	va_list args;
+	int i = 0, j, len = 0;
 
-    return (printed);
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
+Here:
+	while (format[i] != '\0')
+	{
+		j = 13;
+		while (j >= 0)
+		{
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			{
+				len += m[j].f(args);
+				i = i + 2;
+				goto Here;
+			}
+			j--;
+		}
+		_putchar(format[i]);
+		len++;
+		i++;
+	}
+	va_end(args);
+	return (len);
 }
